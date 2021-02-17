ffb87f4777912f5ae886a6e280ee1803a5a51ad4
rosie <rosieontheinternet@gmail.com>
2021-02-16 22:51:40 -0500

    added javascript by adding a hamburger menu

--- script.js
+++ script.js
@@ -2,16 +2,15 @@
 
 // select the menu
 // select the hamburger icon
-// select the X icon
     const nav = document.querySelector('.navigation');
     const hamburgerIcon = document.querySelector('.fa-bars');
 
 //make a click event on the hamburger icon
     hamburgerIcon.addEventListener('click', function(event){
         displayNav(nav);
-        // if (event.target.tagName === 'I') {
-        //     updateHamburger(event.target);
-        // }
+        if (event.target.tagName === 'I') {
+            updateHamburger(event.target);
+        }
     })
 
 //call the function that will bring the hidden menu onto the screen
@@ -19,14 +18,15 @@
         e.classList.toggle('showHamMenu');
     }
 
-//once hamburger icon is clicked, change it to an 'X' 
-    // function updateHamburger(iconE){
-    //     iconE.classList.remove('fa-bars');
-    //     iconE.classList.add('fa-window-close');
-    //     console.log(iconE);
-    // }
 
+//once hamburger icon is clicked, change it to an 'X' 
 //use 'x' icon to close menu
+    function updateHamburger(iconE){
+        iconE.classList.toggle('fa-bars');
+        iconE.classList.toggle('fa-window-close');
+    }
+
+
 
 
 
